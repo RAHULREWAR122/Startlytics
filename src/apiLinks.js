f6f7f21eb138b623eb079080b54239bd7ebd493a
Rahul Rewar <rrewar122@gmail.com>
2025-07-22 15:45:01 +0530

    base url update

--- src/apiLinks.js
+++ src/apiLinks.js
@@ -1,3 +1,3 @@
-
-export const BASE_URL = 'https://myprod.onrender.com'
+// export const BASE_URL = 'https://myprod.onrender.com'
+export const BASE_URL = 'https://myprod-production.up.railway.app'
  
